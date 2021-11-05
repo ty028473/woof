a0078d177c8f9d1cbfb8bb60d93863a98680daa4
danlu <dandesign0818@gmail.com>
2021-11-05 22:51:42 +0800

    更新member改為CSS.Modules版本

--- src/components/member/AsideNavbar.js
+++ src/components/member/AsideNavbar.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import '../../styles/member.scss'
 import { Link } from 'react-router-dom'
+import styles from '../../styles/member.module.scss'
 
-const AsideNavbar = (props) => {
+function AsideNavbar(props) {
   return (
     <>
-      <aside className="col-2">
-        <div className="row flex-column text-center">
-          <div className="col">
-            <img
-              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-              className="img-cover-sm"
-              alt="會員小頭像"
-            />
-          </div>
-          <div className="col">
-            <ul className="list-styled">
-              <li className="p-0">mockup01</li>
-              <li className="pb-3">會員中心</li>
-              <hr />
-            </ul>
-          </div>
+      <div className="row flex-column text-center">
+        <div className="col">
+          <img
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+            className={styles.img_cover_sm}
+            alt="會員小頭像"
+          />
         </div>
-        <ul className="list-styled member-nav">
+        <div className="col">
+          <ul className={styles.list_styled}>
+            <li className="p-0">mockup01</li>
+            <li className="p-0">會員中心</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <div className="mt-3">
+        <ul className={`${styles.list_styled} ${styles.member_nav}`}>
           <li>
             <Link to="/Member">會員資料</Link>
           </li>
@@ -35,10 +35,14 @@ const AsideNavbar = (props) => {
           <li>
             <Link to="/Record">訂單紀錄</Link>
           </li>
-          <li>
+          <li className="p-0">
             <Link to="/Collection">保母收藏</Link>
           </li>
-          <hr className="mb-3" />
+        </ul>
+      </div>
+      <hr />
+      <div className="mt-3">
+        <ul className={`${styles.list_styled} ${styles.member_nav}`}>
           <li>
             <Link to="/PetSitter">切換為保母身分</Link>
           </li>
@@ -46,7 +50,7 @@ const AsideNavbar = (props) => {
             <Link to="/login">登出</Link>
           </li>
         </ul>
-      </aside>
+      </div>
     </>
   )
 }
