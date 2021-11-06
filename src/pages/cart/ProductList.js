e102c4b11431efa1f5042382154c23836811fafa
Anna Liu <a86061827@gmail.com>
2021-11-06 11:14:42 +0800

    增加ProductCheck頁面及其元件，調整cart.scss

--- src/pages/cart/ProductList.js
+++ src/pages/cart/ProductList.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../../styles/golbal.scss'
-import '../../styles/carts.scss'
 import NavBar from '../../components/golbal/NavBar'
 import Footer from '../../components/golbal/Footer'
 import ItemList from '../../components/cart/ItemList'
@@ -9,7 +8,7 @@ function ProductList(props) {
   return (
     <>
       <NavBar />
-      <div className="container height">
+      <div className="container">
         <h3 className="mt-5">購物車</h3>
         <div className="d-flex justify-content-between">
           <h5>勾選下方紅利點數，即可折抵現金！</h5>
@@ -25,7 +24,7 @@ function ProductList(props) {
             </button>
           </form>
         </div>
-        <table className="mt-3 table-title-styled">
+        <table className="mt-3 table-title-styled shadow">
           <tr>
             <td>保母</td>
             <td className="adderss-width">詳細資訊</td>
@@ -42,29 +41,35 @@ function ProductList(props) {
           </button>
         </section> */}
         <ItemList />
-        <div className="check-order cart-shadow">
-          <div class="d-flex bd-highlight ">
-            <div class="py-4 px-5 flex-grow-1 bd-highlight"></div>
-            <div class="py-4 px-5 bd-highlight">
-              <input type="checkbox" className="mr-3" />
-              紅利點數不足
-            </div>
-            <div class="py-4 px-5 bd-highlight col-2">$0</div>
+        <ItemList />
+        <ItemList />
+      </div>
+      {/* check-order */}
+      <div className="check-order cart-shadow">
+        <div class="d-flex bd-highlight ">
+          <div class="py-4 px-5 flex-grow-1 bd-highlight"></div>
+          <div class="py-4 px-5 bd-highlight">
+            <input type="checkbox" className="mr-3" disabled />
+            <span className="none-color">紅利點數不足</span>
           </div>
+          <div class="py-4 px-5 bd-highlight col-2">
+            <span className="none-color">$0</span>
+          </div>
+        </div>
 
-          <hr />
-          <div class="d-flex bd-highlight my-auto">
-            <div class="py-4 px-5 flex-grow-1 bd-highlight"></div>
-            <div class="py-4 px-5 bd-highlight ">總金額 (1個商品):$500</div>
-            <div class="py-4 px-5 bd-highlight col-2">
-              <button type="button" className="btn btn-primary btn-woof">
-                下訂單
-              </button>
-            </div>
+        <hr />
+        <div class="d-flex bd-highlight my-auto">
+          <div class="py-4 px-5 flex-grow-1 bd-highlight"></div>
+          <div class="py-4 px-5 bd-highlight ">
+            總金額 (1個商品): <span className="price-color">$500</span>
+          </div>
+          <div class="py-4 px-5 bd-highlight col-2">
+            <button type="button" className="btn btn-primary btn-woof">
+              下訂單
+            </button>
           </div>
         </div>
       </div>
-
       <Footer />
       <div className="footer-long"></div>
     </>
