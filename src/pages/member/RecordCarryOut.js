dbda02283f1d5eb58ec15528b751f50644d98444
danlu <dandesign0818@gmail.com>
2021-11-07 15:30:04 +0800

    新增訂單紀錄的3個頁面和2個元件和1個css & 更新App.js AsideNavbar.js

---
+++ src/pages/member/RecordCarryOut.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import AsideNavbar from '../../components/member/AsideNavbar'
+import RecordNavbar from '../../components/member/RecordNavbar'
+import RecordData from '../../components/member/RecordData'
+import NavBar from '../../components/golbal/NavBar'
+import Footer from '../../components/golbal/Footer'
+// css
+import styles from '../../styles/record.module.scss'
+
+function RecordCarryOut(props) {
+  return (
+    <>
+      {/* navbar區塊 */}
+      <NavBar />
+      {/* 主要區塊 */}
+      <div className="container">
+        <div className="row m-5">
+          {/* 左側navbar */}
+          <aside className="col-2">
+            <AsideNavbar />
+          </aside>
+          {/* 右側content */}
+          <main className="col-10">
+            <RecordNavbar />
+            {/*功能區塊*/}
+            <div className={`shadow ${styles.frame}`}>
+              <section className="m-3">
+                <div className={`row ${styles.data_block}`}>
+                  <RecordData />
+                  <div className="col-2">
+                    <button
+                      type="button"
+                      className="btn btn-secondary mb-5 btn-woof-Chat"
+                    >
+                      聊聊
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger mt-4 btn-woof-Chat"
+                    >
+                      取消
+                    </button>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </main>
+        </div>
+      </div>
+      {/* footer區塊 */}
+      <Footer />
+    </>
+  )
+}
+
+export default RecordCarryOut
