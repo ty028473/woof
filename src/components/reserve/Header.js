5362db28108d6ba02c038f9ac54972e2aaae41fe
wen1011 <88265377+wen1011@users.noreply.github.com>
2021-11-09 01:08:10 +0800

    修繕篩選

--- src/components/reserve/Header.js
+++ src/components/reserve/Header.js
@@ -7,6 +7,7 @@ import { IconContext } from 'react-icons'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import { Form } from 'react-bootstrap'
 
 export default function Header() {
   return (
@@ -44,57 +45,43 @@ export default function Header() {
       <h1 className="header-title">預約代遛</h1>
       <div className="container filter-title ">
         <div className="row d-flex justify-content-between">
-          <div className="filter-select ">
-            <label className="text-hidden">.</label>
-            <div>
-              <IconContext.Provider value={{ size: 36 }}>
-                <BiCurrentLocation />
-              </IconContext.Provider>
-            </div>
-          </div>
-          <div className="col-2 filter-select">
-            <label>交易地區:</label>
-            <select
-              className="form-select input-filters"
-              aria-label="Default select example"
-            >
-              <option selected>地區範圍</option>
-              <option value="中正區">中正區</option>
-              <option value="大同區">大同區</option>
-              <option value="中山區">中山區</option>
-              <option value="松山區">松山區</option>
-              <option value="大安區">大安區</option>
-              <option value="萬華區">萬華區</option>
-              <option value="信義區">信義區</option>
-              <option value="士林區">士林區</option>
-              <option value="北投區">北投區</option>
-              <option value="內湖區">內湖區</option>
-              <option value="南港區">南港區</option>
-              <option value="文山區">文山區</option>
-            </select>
+          <div>
+            <IconContext.Provider value={{ size: 36 }}>
+              <BiCurrentLocation />
+            </IconContext.Provider>
           </div>
 
-          <div className="col-2 filter-select">
-            <div className="filter-select">
-              <label>預約日期:</label>
-              <input className="input-filters" type="date" autoComplete="off" />
+          <div className="col-2 ">
+            <div className="input-group">
+              <select class="custom-select">
+                <option selected>服務地區</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
             </div>
           </div>
-          <div className="col-2 filter-select ">
-            <label>預約時段:</label>
-            <select
-              className="form-select input-filters"
-              aria-label="Default select example"
-            >
-              <option selected>時段範圍</option>
-              <option value={1}>08:00~12:00</option>
-              <option value={2}>12:00~16:00</option>
-              <option value={3}>16:00~20:00</option>
-            </select>
+          <div className="col-3 ">
+            <Form.Group controlId="dob">
+              <Form.Control
+                type="date"
+                name="dob"
+                placeholder="Date of Birth"
+              />
+            </Form.Group>
+          </div>
+          <div className="col-2 ">
+            <div className="input-group">
+              <select class="custom-select">
+                <option selected>時段</option>
+                <option value="1">08:00-12:00</option>
+                <option value="2">12:00-16:00</option>
+                <option value="3">12:00-20:00</option>
+              </select>
+            </div>
           </div>
 
-          <div className="col-5 ">
-            <label className="text-hidden">.</label>
+          <div className="col-4 ">
             <InputGroup>
               <FormControl
                 placeholder="想找什麼呢？"
