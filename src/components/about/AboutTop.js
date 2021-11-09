c185a2073ecb5df9fb362d4d57f1e6310da7d665
ty028473 <fgh1887763145@gmail.com>
2021-11-09 08:38:29 +0800

    修正浮水印

--- src/components/about/AboutTop.js
+++ src/components/about/AboutTop.js
@@ -6,17 +6,17 @@ function AboutTop(props) {
     <>
       <div className="abTop">
         <div className="abToptext">
-          <h1 className="mb-4" >狗的脚印與心相連</h1>
-          <p className="abtopp">查找並預訂您所在城市的當地寵物保姆、遛狗者和寵物服務者</p>
+          <h1 className="mb-4">狗的脚印與心相連</h1>
+          <p className="abtopp">
+            查找並預訂您所在城市的當地寵物保姆、遛狗者和寵物服務者
+          </p>
         </div>
         <div className="abImgcover">
-          <img className="abImg" src="../images/about/1.png" alt="" />
+          <img className="abImg" src="../images/about/1.jpeg" alt="" />
         </div>
       </div>
-   
-        
+
       <h3 className="mt-3 mb-3 text-center">關於woof</h3>
-      
     </>
   )
 }
