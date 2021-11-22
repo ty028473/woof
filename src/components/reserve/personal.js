70e36dcfb16c0ce7ab3ab701360f0654007756a7
wen1011 <88265377+wen1011@users.noreply.github.com>
2021-11-22 16:31:54 +0800

    預約串接

--- src/components/reserve/personal.js
+++ src/components/reserve/personal.js
@@ -1,34 +1,56 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import personal from '../../local-json/personal.json'
 import '../../styles/calender.scss'
 import { faStar, faMapMarker } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from 'axios'
 
 function SitterDetail(props) {
-  const [personalData, setPersonalState] = React.useState(personal)
+  const [personalData, setPersonalState] = useState()
+  const { reserveId } = useParams()
+  useEffect(async () => {
+    let res = await axios.get(`http://localhost:8801/api/Reserve/${reserveId}`)
+
+    setPersonalState(res.data)
+  }, [])
+
   return (
     <>
-      {personalData &&
-        personalData.map(({ image, name, district, evaluation, score, id }) => (
-          <table className=" list-styled">
-            <tr key={id}>
-              <td>
-                <img className="profile-img-cover" src={image} alt="img" />
-              </td>
+      {
+        <table className=" list-styled">
+          <tr key={personalData && personalData.id}>
+            <td>
+              <img
+                className="profile-img-cover"
+                src={personalData && personalData.image}
+                alt="img"
+              />
+            </td>
 
-              <td className="proflie-width ">
-                <h3>{name}</h3>
-                <FontAwesomeIcon icon={faStar} className="sitter-icon mr-1" />
-                {score}
-                <br />
-                <p className="evaluation-font">{evaluation}</p>
-                <br />
-                <FontAwesomeIcon icon={faMapMarker} className="mr-1" />
-                {district}
-              </td>
-            </tr>
-          </table>
-        ))}
+            <td className="proflie-width ">
+              <h3>{personalData && personalData.name}</h3>
+              <FontAwesomeIcon icon={faStar} className="sitter-icon mr-1" />
+              {personalData && personalData.score}
+              <br />
+              <p className="evaluation-font">
+                {personalData && personalData.evaluation}
+              </p>
+
+              <FontAwesomeIcon icon={faMapMarker} className="mr-1" />
+              {personalData && personalData.district}
+              <br />
+              <button
+                type="button"
+                className="btn btn-primary btn-sm btn-woof mr-1 mt-2"
+              >
+                收藏
+              </button>
+            </td>
+          </tr>
+        </table>
+      }
 
       <button
         type="button"
