0623ab7ff92a55ffd8b6dae98b0ad79b3c97441e
danlu <dandesign0818@gmail.com>
2021-11-30 10:34:38 +0800

    新增登入後存取localstorage

--- src/components/golbal/NavBar.js
+++ src/components/golbal/NavBar.js
@@ -2,12 +2,30 @@ import { BrowserRouter as Link } from 'react-router-dom'
 import React, { useContext } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { BsFillCartFill } from 'react-icons/bs'
+// context
 import { ProductContext } from '../../contexts/ProductContext'
+import { UserContext } from '../../contexts/UserContext'
+import axios from 'axios'
+import { API_URL } from '../../configs/Config'
+
+// css
 import '../../styles/golbal.scss'
 import '../../styles/navbar-footer.scss'
 
-function NavBar(props) {
+function NavBar() {
   const { products } = useContext(ProductContext)
+  const { memberSession } = useContext(UserContext)
+  console.log('Navbar.js', memberSession)
+
+  // 登出
+  async function handleLogout() {
+    try {
+      localStorage.removeItem('member')
+      await axios.get(`${API_URL}/auth/logout`, { withCredentials: true })
+    } catch (e) {
+      console.log(e)
+    }
+  }
   return (
     <>
       <Navbar fixed="top" sticky="top" bg="secondary" variant="dark">
@@ -29,24 +47,46 @@ function NavBar(props) {
             成為保母
           </Nav.Link>
         </Nav>
-        <Nav className="mr-right flex-right">
-          <Nav.Link className="icon-cart" href="/cart">
-            <BsFillCartFill className="mr-2" />
+        {memberSession ? (
+          <Nav className="mr-right flex-right">
+            <Nav.Link className="icon-cart" href="/cart">
+              <BsFillCartFill className="mr-2" />
+              {products.length ? (
+                <button className="badge">{products.length}</button>
+              ) : (
+                ''
+              )}
+            </Nav.Link>
+            <Nav.Link className="nav-title" href="/member">
+              會員中心
+            </Nav.Link>
+            <Nav.Link
+              className="nav-title"
+              href="/login"
+              onClick={handleLogout}
+            >
+              登出
+            </Nav.Link>
+          </Nav>
+        ) : (
+          <Nav className="mr-right flex-right">
+            <Nav.Link className="icon-cart" href="/cart">
+              <BsFillCartFill className="mr-2" />
 
-            {products.length ? (
-              <button className="badge">{products.length}</button>
-            ) : (
-              ''
-            )}
-          </Nav.Link>
-          
-          <Nav.Link className="nav-title" href="/login">
-            登入
-          </Nav.Link>
-          <Nav.Link className="nav-title" href="/signup">
-            註冊
-          </Nav.Link>
-        </Nav>
+              {products.length ? (
+                <button className="badge">{products.length}</button>
+              ) : (
+                ''
+              )}
+            </Nav.Link>
+            <Nav.Link className="nav-title" href="/login">
+              登入
+            </Nav.Link>
+            <Nav.Link className="nav-title" href="/signup">
+              註冊
+            </Nav.Link>
+          </Nav>
+        )}
       </Navbar>
     </>
   )
