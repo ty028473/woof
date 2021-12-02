68cfc0c357f59ab76d2e1a0deedcc703ddf63d93
Anna Liu <a86061827@gmail.com>
2021-12-02 15:04:12 +0800

    調整首頁動畫＋測試購物車小視窗

--- src/components/golbal/CartList.js
+++ src/components/golbal/CartList.js
@@ -1,42 +1,29 @@
-import React from 'react'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useContext } from 'react'
 import '../../styles/golbal.scss'
 import '../../styles/cartList.scss'
+import { ProductContext } from '../../contexts/ProductContext'
+import ProductListDetail from './ProductListDetail'
 
 function CartList(props) {
+  const { products } = useContext(ProductContext)
 
-
-  return (
+  return products.length ? (
     <>
-      <div className="row my-1 px-3 cart-list-styled shadow">
-        <div className="col-2 my-auto">
-          <h6>name</h6>
-          <img src="" className="img-cover-sm-square-cartList" alt="" />
-        </div>
-        <div className="col-4 my-auto">
-          交易地點： <br />
-          寵物名稱：
-        </div>
-        <div className="col-3 my-auto">
-          開始時間
-          <br />
-          結束時間
-        </div>
-        <div className="col-2 my-auto">
-          <span className="price-color">$100</span>
-        </div>
-        <div className="col-1 my-auto">
-          <FontAwesomeIcon
-            icon={faTrash}
-            size="lg"
-            // onClick={() =>
-            //   dispatch({ type: 'REMOVE_PRODUCT', id: product.id })
-            // }
-          />
-        </div>
-      </div>
+      {products.map((product) => {
+        return <ProductListDetail product={product} key={product.id} />
+      })}
     </>
+  ) : (
+    <div className="cart-list-styled">
+      <section className="cart-list-item text-center">
+        <h5 className="pt-1">購物車是空的！趕快去逛逛吧～</h5>
+        <a href="http://localhost:3000/ReserveMap">
+          <button type="button" className="btn btn-primary btn-woof">
+            點我預約去～
+          </button>
+        </a>
+      </section>
+    </div>
   )
 }
 
