76f6adb472bae19b9695cfc1ba8df6039a70c1d7
danlu <dandesign0818@gmail.com>
2021-12-04 13:44:50 +0800

    新增訂單

--- src/pages/member/Pets.js
+++ src/pages/member/Pets.js
@@ -23,7 +23,7 @@ function Pets() {
       image: '',
     },
   ])
-
+  console.log(petData)
   // 抓取寵物資料
   useEffect(() => {
     async function getPetData() {
@@ -52,8 +52,32 @@ function Pets() {
         </section>
         <hr />
         {/*功能區塊*/}
-        <section className="m-4">
-          {petData.map((v) => {
+
+        {petData[0].name === '' ? (
+          <section>
+            <div className="row align-items-center">
+              <div className="col">
+                <h3>您目前尚無寵物</h3>
+              </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </section>
+        ) : (
+          petData.map((v) => {
             // 顯示性別
             let handleGender
             if (v.gender === 1) {
@@ -64,86 +88,87 @@ function Pets() {
               handleGender = '不顯示'
             }
             return (
-              <div
-                className={`row my-3 position-relative ${userGlobal.data_block}`}
-                key={v.id}
-              >
-                {/* <div className="col-3 text-right"> */}
-                <div className={`col-3 position-relative`}>
-                  <div className={userGlobal.img_center}>
-                    <img
-                      src={`${PUBLIC_URL}${v.image}`}
-                      className={userGlobal.img_cover_square_sm}
-                      alt="寵物頭像"
-                    />
-                  </div>
-                </div>
-
-                <div className="col-3 text-center">
-                  <ul
-                    className={`${userGlobal.list_styled} ${userGlobal.title_font}`}
-                  >
-                    <li className="fontsize">寵物名稱</li>
-                    <li>寵物性別</li>
-                    <li>寵物歲數</li>
-                    <li>詳細資料</li>
-                  </ul>
-                </div>
-                <div className="col-6 text-left">
-                  <ul
-                    className={`${userGlobal.list_styled} ${userGlobal.content_font}`}
-                  >
-                    <li>{v.name}</li>
-                    <li>{handleGender}</li>
-                    <li>{moment().format('YYYY') - v.birthday}</li>
-                    <li>{v.information}</li>
-                  </ul>
-                </div>
-
-                <Link
-                  to={{ pathname: '/member/pets/update', state: { data: v } }}
+              <section className="m-4">
+                <div
+                  className={`row my-3 position-relative ${userGlobal.data_block}`}
+                  key={v.id}
                 >
-                  <div className={userGlobal.icon_edit}>
+                  {/* <div className="col-3 text-right"> */}
+                  <div className={`col-3 position-relative`}>
+                    <div className={userGlobal.img_center}>
+                      <img
+                        src={`${PUBLIC_URL}${v.image}`}
+                        className={userGlobal.img_cover_square_sm}
+                        alt="寵物頭像"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-3 text-center">
+                    <ul
+                      className={`${userGlobal.list_styled} ${userGlobal.title_font}`}
+                    >
+                      <li className="fontsize">寵物名稱</li>
+                      <li>寵物性別</li>
+                      <li>寵物歲數</li>
+                      <li>詳細資料</li>
+                    </ul>
+                  </div>
+                  <div className="col-6 text-left">
+                    <ul
+                      className={`${userGlobal.list_styled} ${userGlobal.content_font}`}
+                    >
+                      <li>{v.name}</li>
+                      <li>{handleGender}</li>
+                      <li>{moment().format('YYYY') - v.birthday}</li>
+                      <li>{v.information}</li>
+                    </ul>
+                  </div>
+
+                  <Link
+                    to={{ pathname: '/member/pets/update', state: { data: v } }}
+                  >
+                    <div className={userGlobal.icon_edit}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        size="lg"
+                        className={userGlobal.icon_cursor}
+                      />
+                    </div>
+                  </Link>
+
+                  <div className={userGlobal.icon_trash}>
                     <FontAwesomeIcon
-                      icon={faEdit}
+                      icon={faTrash}
                       size="lg"
                       className={userGlobal.icon_cursor}
+                      onClick={async () => {
+                        try {
+                          let res = await axios.post(
+                            `${API_URL}/pet/deletePet`,
+                            v,
+                            {
+                              withCredentials: true,
+                            }
+                          )
+                          swal({
+                            title: '寵物刪除成功',
+                            text: ' ',
+                            icon: 'success',
+                            buttons: false,
+                            timer: 1000,
+                          }).then(() => {
+                            let index = petData.indexOf(v)
+                            setPetData(petData.splice(index, 1))
+                          })
+                        } catch (err) {
+                          console.log(err)
+                        }
+                      }}
                     />
                   </div>
-                </Link>
 
-                <div className={userGlobal.icon_trash}>
-                  <FontAwesomeIcon
-                    icon={faTrash}
-                    size="lg"
-                    className={userGlobal.icon_cursor}
-                    onClick={async () => {
-                      try {
-                        let res = await axios.post(
-                          `${API_URL}/pet/deletePet`,
-                          v,
-                          {
-                            withCredentials: true,
-                          }
-                        )
-                        swal({
-                          title: '寵物刪除成功',
-                          text: ' ',
-                          icon: 'success',
-                          buttons: false,
-                          timer: 1000,
-                        }).then(() => {
-                          let index = petData.indexOf(v)
-                          setPetData(petData.splice(index, 1))
-                        })
-                      } catch (err) {
-                        console.log(err)
-                      }
-                    }}
-                  />
-                </div>
-
-                {/* <div className="col-3">
+                  {/* <div className="col-3">
                   <div className="row">
                     <div className="col text-right">
                       <FontAwesomeIcon icon={faEdit} size="lg" />
@@ -153,10 +178,11 @@ function Pets() {
                     </div>
                   </div>
                 </div> */}
-              </div>
+                </div>
+              </section>
             )
-          })}
-        </section>
+          })
+        )}
       </GlobalJsx>
     </>
   )
--- src/pages/member/RecordCancel.js
+++ src/pages/member/RecordCancel.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GlobalJsx from '../../components/member/GlobalJsx'
 import RecordGlobalJsx from '../../components/member/RecordGlobalJsx'
+import axios from 'axios'
+import swal from 'sweetalert'
+import { API_URL } from '../../configs/Config'
 
 function RecordCancel() {
+  const [memberRecordData, setMemberRecordData] = useState([
+    {
+      petSitterName: '',
+      petSitterImage: '',
+      district: '',
+      address: '',
+      start: '',
+      end: '',
+      price: 0,
+      petName: '',
+    },
+  ])
+  console.log('memberRecordData', memberRecordData)
+
+  // 抓取會員訂單進行中資料
+  useEffect(() => {
+    async function getMemberRecordData() {
+      let res = await axios.get(`${API_URL}/memberRecord/cancel`, {
+        withCredentials: true,
+      })
+      setMemberRecordData(res.data)
+    }
+    getMemberRecordData()
+  }, [])
   return (
     <>
       <GlobalJsx>
-        <RecordGlobalJsx></RecordGlobalJsx>
+        <RecordGlobalJsx memberRecordData={memberRecordData} />
       </GlobalJsx>
     </>
   )
