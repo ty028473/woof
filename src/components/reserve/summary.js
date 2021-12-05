3e8e410a0466b39701f5aeaf9100b332fb2eb4fe
wen1011 <88265377+wen1011@users.noreply.github.com>
2021-12-05 13:08:30 +0800

    修改保母簡歷

--- src/components/reserve/summary.js
+++ src/components/reserve/summary.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import summary from '../../local-json/summary.json'
+
 import '../../styles/calender.scss'
 import axios from 'axios'
 import { API_URL } from '../../configs/Config'
